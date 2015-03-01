cacf6a014c8ec6550af04b9e517de468bf28eed7
Cafer Şimşek <cafer.simsek@cimri.com>
2015-03-01 19:18:06 +0200

    some utility functions

---
+++ utils.c
@@ -0,0 +1,33 @@
+#include "fio.h"
+
+int mkdir_recursive(const char *dir, mode_t mode) {
+    int result;
+    char tmp[256];
+    char *p = NULL;
+    size_t len;
+    snprintf(tmp, sizeof(tmp),"%s",dir);
+    len = strlen(tmp);
+    if(tmp[len - 1] == '/') {
+        tmp[len - 1] = 0;
+    }
+    for(p = tmp + 1; *p; p++) {
+        if(*p == '/') {
+            *p = 0;
+            mkdir(tmp, mode);
+            chmod(tmp, mode);
+            *p = '/';
+        }
+    }
+    result = mkdir(tmp, mode);
+    chmod(tmp, mode);
+    return result;
+}
+
+long getfilesize(FILE *fd) {
+	long result;
+	long oldpos = ftell(fd);
+	fseek(fd, 0, SEEK_END);
+    result = ftell(fd);
+    fseek(fd, oldpos, SEEK_SET);
+    return result;
+}
---
+++ utils.h
@@ -0,0 +1,5 @@
+#ifndef __UTILS_H
+#define __UTILS_H
+int mkdir_recursive(const char *dir, mode_t mode);
+long getfilesize(FILE *fd);
+#endif
